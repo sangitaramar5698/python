77adda4aae6b404e297568d1f75e1b63a00ca216
sangitaramar5698 <31088882+sangitaramar5698@users.noreply.github.com>
2017-10-20 20:08:35 +0530

    Create math_opertn_num.py

---
+++ math_opertn_num.py
@@ -0,0 +1,17 @@
+#python code goes here
+#python version :3.2.5
+a1=5
+a2=10
+a3=15
+a4=20
+a5=25
+a6=30
+a7=35
+a8=40
+a9=45
+a10=50
+print "a1+a2+a3"
+print "a10-a1"
+print "a1*a3*a5"
+print "a10/a2"
+print "a10%a3"
