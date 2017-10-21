7cdf3c0cabdf391cdd9a8d7c6070a10ae3a2559e
sangitaramar5698 <31088882+sangitaramar5698@users.noreply.github.com>
2017-10-21 12:03:57 +0530

    Create type_of_data.py

---
+++ type_of_data.py
@@ -0,0 +1,10 @@
+def main():
+    num1 = 1
+    num2 = 6.2687629678523853
+    n1="ste"
+    n2=-6567657889.8978967564534453452334232573465478676
+    print(type(num1))
+    print(type(num2))
+    print(type(n1))
+    print(type(n2))
+
