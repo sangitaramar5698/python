400a514c41c41721c1526c3d14bb94ed994bf018
sangitaramar5698 <31088882+sangitaramar5698@users.noreply.github.com>
2017-10-21 12:09:44 +0530

    Update type_of_data.py

--- type_of_data.py
+++ type_of_data.py
@@ -3,8 +3,9 @@ def main():
     num2 = 6.2687629678523853
     n1="ste"
     n2=-6567657889.8978967564534453452334232573465478676
+    x = 5 + 2j
     print(type(num1))
     print(type(num2))
     print(type(n1))
     print(type(n2))
-
+    print(type(x))
