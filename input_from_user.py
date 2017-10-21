337604080f8fb06f223bfbad35a4edcb62e8d596
sangitaramar5698 <31088882+sangitaramar5698@users.noreply.github.com>
2017-10-21 14:01:42 +0530

    Create input_from_user.py

---
+++ input_from_user.py
@@ -0,0 +1,8 @@
+def main():
+    a=raw_input()
+    b=raw_input()
+    print(int(a)+int(b))
+    g=raw_input()
+    c=len(str(g))
+    print(int(c))
+    #it prints the sum of the two inputs given from the user and also print the length of the string.
