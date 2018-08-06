cd8424d5bdd35313d6f0a8cc38f07fcbedae4902
sangitaramar5698 <31088882+sangitaramar5698@users.noreply.github.com>
2018-08-06 12:14:54 +0530

    Update odd_even.py

--- odd_even.py
+++ odd_even.py
@@ -2,7 +2,7 @@
 num = int(input("Enter a number: "))
 if (num % 2)== 0:
    print("Even number")
-elif num == 0:
-   print("number is zero")
+elif num <= 0:
+   print("number is invalid")
 else:
    print("odd number")
